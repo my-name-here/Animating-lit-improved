9c75db60fd401e88da90708ff19058b8e7c1c361
my-name-here <christopherbrower@proofschool.org>
2020-09-21 12:28:36 -0700

    Update scroll_change.js
    add logic

--- scroll_change.js
+++ scroll_change.js
@@ -5,6 +5,16 @@ function getScrollPercent() {
         sh = 'scrollHeight';
     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
 }
-var distance = getScrollPercent()
-var current_page="poem.html";
-document.getElementById('the_poem').src=current_page ;
+window.onscroll = function change(){
+    var distance = getScrollPercent()
+    var current_page="poem.html";
+    if distance>=0 && distance<25{
+        var current_page="layer_1.html";}
+    if distance>=25 && distance<50{
+        var current_page="layer_2.html";}
+    if distance>=50 && distance<75{
+        var current_page="layer_3.html";}
+    if distance>=75 && distance<=100{
+        var current_page="layer_4.html";}
+    document.getElementById('the_poem').src=current_page ;
+}
