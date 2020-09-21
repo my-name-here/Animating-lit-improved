e689330e254bbbbc5925627d80bbad46bacbab8f
my-name-here <christopherbrower@proofschool.org>
2020-09-21 11:55:51 -0700

    Update scroll_change.js

--- scroll_change.js
+++ scroll_change.js
@@ -1,1 +1,10 @@
-var current_page = document.getElementById('the_poem').src;
+function getScrollPercent() {
+    var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+}
+var distance = getScrollPercent()
+var current_page="poem.html";
+document.getElementById('the_poem').src=current_page ;
